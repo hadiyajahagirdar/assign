eed572fc289d91f9b68febbc879c515b7332babe
hadiyajahagirdar <115102788+hadiyajahagirdar@users.noreply.github.com>
2023-04-11 05:11:55 +0530

    assign

---
+++ assign/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Latitude from './components/Latitude';
+
+
+function App() {
+  return (
+    <div>
+     <Latitude/>
+
+    </div>
+          
+  );
+}
+
+export default App;
---
+++ assign/src/components/Latitude.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Latitude() {
+  return (
+    <div classNameName='container' >
+      <ul className="nav">
+  <li className="nav-item">
+    <a className="nav-link " aria-current="page" href="https://www.apple.com/in/pages/">Home</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="https://pages.skydocu.com/en/edit-text/add-text/">About</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="https://support.apple.com/en-in/guide/pages/welcome/mac">Features</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link active" href="https://pages.skydocu.com/en/edit-text/add-text/" tabindex="-1" aria-disabled="true">Go here for pages</a>
+  </li>
+</ul>
+    </div>
+  )
+}
